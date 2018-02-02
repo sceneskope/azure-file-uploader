7236e35fa2c11650be018ab58914aaa35677fcaf
Nick Randell <nick.randell@sceneskope.com>
2018-02-02 12:23:31 +0000

    Extracted from internal source

---
+++ UploadFiles/Arguments.cs
@@ -0,0 +1,44 @@
+﻿using CommandLineParser.Arguments;
+using SceneSkope.Utilities.CommandLineApplications;
+
+namespace UploadFiles
+{
+    internal class Arguments : ArgumentsBase
+    {
+        [ValueArgument(typeof(string), 'k', "key", Description = "Application insights key", Optional = true)]
+        public override string Key { get; set; }
+
+        [ValueArgument(typeof(string), 'q', "seqhost", Description = "Seq server host", Optional = true)]
+        public override string SeqHost { get; set; }
+
+        [ValueArgument(typeof(string), 'o', "seqtoken", Description = "Seq token", Optional = true)]
+        public override string SeqToken { get; set; }
+
+        [ValueArgument(typeof(string), 'l', "lockfile", Description = "Lock file", Optional = true)]
+        public override string LockFile { get; set; }
+
+        [SwitchArgument('h', "help", false, Description = "Show usage", Optional = true)]
+        public override bool Help { get; set; }
+
+        [SwitchArgument('c', "noconsole", false, Description = "No console", Optional = true)]
+        public override bool NoConsole { get; set; }
+
+        [ValueArgument(typeof(string), "input", Description = "Input directory", Optional = false)]
+        public string InputDirectory { get; set; }
+
+        [ValueArgument(typeof(string), "storageAccount", Description = "Storage account name", Optional = false)]
+        public string AccountName { get; set; }
+
+        [ValueArgument(typeof(string), "storageKeyValue", Description = "Storage key value", Optional = false)]
+        public string KeyValue { get; set; }
+
+        [ValueArgument(typeof(string), "storageContainer", Description = "Storage container name", Optional = false)]
+        public string ContainerName { get; set; }
+
+        [ValueArgument(typeof(string), "matchingRegex", Description = "Matching regular expression", Optional = false)]
+        public string MatchingRegex { get; set; }
+
+        [ValueArgument(typeof(string), "logfile", Description = "Log file name including the {Date} template", Optional = true)]
+        public override string LogFile { get; set; }
+    }
+}
---
+++ UploadFiles/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.DataMovement;
+using SceneSkope.Utilities.CommandLineApplications;
+using Serilog;
+
+namespace UploadFiles
+{
+    internal class Program : ApplicationBase<Arguments>
+    {
+        private static void Main(string[] args) => new Program().ApplicationMain(args);
+        protected override async Task RunAsync(Arguments arguments, CancellationToken ct)
+        {
+            var container = GetContainer(arguments);
+            var originalLog = Log.Logger;
+            Log.Logger = originalLog.ForContext("StorageAccount", arguments.AccountName).ForContext("StorageContainer", arguments.ContainerName);
+            await container.CreateIfNotExistsAsync(BlobContainerPublicAccessType.Off, null, null, ct).ConfigureAwait(false);
+            Log.Information("Looking for matching files");
+
+            var allFiles = GetFilesToTransfer(arguments);
+            Log.Information("Found {Count} files that match", allFiles.Count);
+            var transferTasks = CreateTransferTasks(container, allFiles, ct);
+            await WaitForAllTransfersToFinishAsync(allFiles, transferTasks, ct).ConfigureAwait(false);
+            Log.Logger = originalLog;
+            Log.CloseAndFlush();
+        }
+
+        private static async Task WaitForAllTransfersToFinishAsync(List<(FileInfo file, Match match)> allFiles, List<Task> transferTasks,
+            CancellationToken ct)
+        {
+            var totalCount = allFiles.Count;
+            var finishedCount = 0;
+            var skippedCount = 0;
+            var successCount = 0;
+            var errorCount = 0;
+            while (transferTasks.Count > 0)
+            {
+                var finished = await Task.WhenAny(transferTasks).ConfigureAwait(false);
+                ct.ThrowIfCancellationRequested();
+                finishedCount++;
+                var index = transferTasks.IndexOf(finished);
+                var (file, match) = allFiles[index];
+                allFiles.RemoveAt(index);
+                transferTasks.RemoveAt(index);
+                if (finished.IsFaulted)
+                {
+                    switch (finished.Exception.InnerException)
+                    {
+                        case TransferSkippedException tse:
+                            Log.Debug("Skipped {File}", file.Name);
+                            skippedCount++;
+                            break;
+                        default:
+                            Log.Warning(finished.Exception, "Error in {File}: {Exception}", file.Name, finished.Exception.Message);
+                            errorCount++;
+                            break;
+                    }
+                }
+                else
+                {
+                    Log.Information("Transferred {File} ok", file.Name);
+                    successCount++;
+                }
+            }
+            Log.Information("Transferred {SuccessCount} ok, skipped {SkippedCount}, had {ErrorsCount} errors",
+                successCount, skippedCount, errorCount);
+        }
+
+        private static List<Task> CreateTransferTasks(CloudBlobContainer container, List<(FileInfo file, Match match)> allFiles, CancellationToken ct)
+        {
+            TransferManager.Configurations.ParallelOperations = 64;
+            var transferTasks = new List<Task>(allFiles.Count);
+
+            foreach (var (file, match) in allFiles)
+            {
+                var context = new SingleTransferContext
+                {
+                    ShouldOverwriteCallback = ShouldOverWriteCallback
+                };
+                var uploadOptions = new UploadOptions();
+                var targetName = file.Name;
+                var targetBlob = container.GetBlockBlobReference(targetName);
+                var task = TransferManager.UploadAsync(file.FullName, targetBlob, uploadOptions, context, ct);
+                transferTasks.Add(task);
+            }
+
+            return transferTasks;
+        }
+
+        private static List<(FileInfo file, Match match)> GetFilesToTransfer(Arguments arguments)
+        {
+            var inputDirectory = new DirectoryInfo(arguments.InputDirectory);
+            var matchingRegex = new Regex(arguments.MatchingRegex, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var allFiles = inputDirectory
+                .EnumerateFiles("*", SearchOption.AllDirectories)
+                .Select(file =>
+                {
+                    var match = matchingRegex.Match(file.Name);
+                    return (file, match);
+                })
+                .Where(fm => fm.match.Success)
+                .OrderBy(fm => fm.match.Groups["sorter"].Value)
+                .ToList();
+            return allFiles;
+        }
+
+        private static CloudBlobContainer GetContainer(Arguments arguments)
+        {
+            var credentials = new StorageCredentials(arguments.AccountName, arguments.KeyValue);
+            var account = new CloudStorageAccount(credentials, true);
+            var client = account.CreateCloudBlobClient();
+            var container = client.GetContainerReference(arguments.ContainerName);
+            return container;
+        }
+
+        private static bool ShouldOverWriteCallback(object source, object destination)
+        {
+            var sourceFileName = (string)source;
+            var destinationBlob = (CloudBlockBlob)destination;
+            var sourceFile = new FileInfo(sourceFileName);
+            if (sourceFile.Length == 0)
+            {
+                Log.Information("Not transferring zero length {File}", sourceFileName);
+                return false;
+            }
+            else
+            {
+                var sourceMd5 = Task.Factory.StartNew(() => CalculateMD5Async(sourceFileName, default)).Unwrap().GetAwaiter().GetResult();
+                return sourceMd5 != destinationBlob.Properties.ContentMD5;
+            }
+        }
+
+        public static async Task<string> CalculateMD5Async(string fullFileName, CancellationToken ct)
+        {
+            var block = ArrayPool<byte>.Shared.Rent(8192);
+            try
+            {
+                using (var md5 = MD5.Create())
+                {
+                    using (var stream = new FileStream(fullFileName, FileMode.Open, FileAccess.Read, FileShare.Read, 8192, true))
+                    {
+                        int length;
+                        while ((length = await stream.ReadAsync(block, 0, block.Length).ConfigureAwait(false)) > 0)
+                        {
+                            md5.TransformBlock(block, 0, length, null, 0);
+                            ct.ThrowIfCancellationRequested();
+                        }
+                        md5.TransformFinalBlock(block, 0, 0);
+                    }
+                    var hash = md5.Hash;
+                    return Convert.ToBase64String(hash);
+                }
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(block);
+            }
+        }
+    }
+}
